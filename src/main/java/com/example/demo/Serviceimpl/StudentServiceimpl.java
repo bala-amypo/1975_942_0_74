8958efa16944242dc2d51a58112c495a5033b788
bala-amypo <bala@amypo.in>
2025-12-17 09:41:03 +0000

    User commit

--- src/main/java/com/example/demo/Serviceimpl/StudentServiceimpl.java
+++ src/main/java/com/example/demo/Serviceimpl/StudentServiceimpl.java
@@ -4,10 +4,13 @@ import com.example.demo.service.StudentService;
 
 @Service
 public class StudentServiceImpl implements StudentService{
-    private final StudentRep
-
+    private final StudentRepository studentRepository;
+    public StudentServiceImpl(StudentRepository studentRepository){
+        this.studentRepository=studentRepository;
+    }
+ 
 
     public Student saveStudent(Student student){
-        return 
+        return  stud
     }
 }
